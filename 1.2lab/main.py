807a038ccc19a06c177c081c400705deb94545e8
syrrr77 <159061724+syrrr77@users.noreply.github.com>
2025-11-05 22:06:45 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+# Ввод значений A, B и H
+A = float(input("Введите значение A: "))
+B = float(input("Введите значение B: "))
+H = float(input("Введите шаг H: "))
+
+# Итерация по диапазону с шагом H
+now = A
+while now <= B:
+    if now > 0:
+        print(now)
+    now += H
