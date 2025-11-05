386e6967c5d41dac16b9e797262367f3d081cc03
syrrr77 <159061724+syrrr77@users.noreply.github.com>
2025-11-05 22:59:33 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,17 @@
-if __name__ == "__main__":
-    pass
+# Создание списка
+ronaldo = [int(x) for x in input("Введите элементы списка через пробел: ").split()]
+
+# Находим максимум и его индекс в списке
+max_ron= max(ronaldo)
+max_index = ronaldo.index(max_ron)
+
+# Получаем список элементов справа от максимального без него
+right_part = ronaldo[max_index+ 1:]
+
+# Вычисляем произведение элементов справа
+product= 1
+for num in right_part:
+    product*=num
+
+# Если справа нет элементов, произведение = 1
+print("Произведение элементов справа от максимального = :", product)
