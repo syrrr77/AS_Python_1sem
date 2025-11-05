59e39fd330537f271b1c496c133253dd73cdd865
syrrr77 <159061724+syrrr77@users.noreply.github.com>
2025-11-05 22:26:00 +0300

    Update main.py

--- 1.6lab/main.py
+++ 1.6lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+# Ввод строки и значения n
+cr7 = input("Введите строку: ")
+n = int(input("Введите n: "))
+
+# Создаём новую строку, пропуская  n-ые символы
+final = ""
+
+for i in range(len(cr7)):
+    if (i + 1) % n != 0:
+        final += cr7[i]
+
+print("Результат: ", final)
