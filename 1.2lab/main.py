d4876750f0a6aacd6a54428d8ec5f6b00d9e8079
syrrr77 <159061724+syrrr77@users.noreply.github.com>
2025-11-05 23:03:47 +0300

    Update main.py

--- 1.2lab/main.py
+++ 1.2lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+# Ввод значений A, B и H
+A = float(input("Введите A: "))
+B = float(input("Введите B: "))
+H = float(input("Введите H: "))
+
+# Проходим по диапазону и выводим только положительные числа
+x = A
+while x <= B:
+    if x > 0:
+        print(x)
+    x += H
